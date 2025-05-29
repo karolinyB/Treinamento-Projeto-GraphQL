e82bd18e4fd386f26bb6fe75c4ffcaa80349d17d
karolinyB <ana.silva@baymetrics.com.br>
2025-05-29 19:46:18 -0300

    feat: criacao do diretório db

---
+++ src/db/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+  usuarios: [
+    {
+      id: 1,
+      nome: "Ana",
+      email: "aninha@gmail.com",
+      telefone_fixo: "34 1234 5602",
+      perfil: 1,
+    },
+    {
+      id: 2,
+      nome: "Joana",
+      email: "joaninha@gmail.com",
+      telefone_fixo: "34 1234 5129",
+      perfil: 2,
+    },
+    {
+      id: 3,
+      nome: "Helena",
+      email: "heleninha@gmail.com",
+      telefone_fixo: "34 1234 5659",
+      perfil: 2,
+    },
+  ],
+
+  perfis: [
+    { id: 1, tipo: "ADMIN" },
+    { id: 2, tipo: "COMUM" },
+  ],
+};
